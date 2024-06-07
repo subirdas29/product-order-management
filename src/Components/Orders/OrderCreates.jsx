81a7d9bf5a05bec61ce798430d2bff7b55984d34
Subir Das <dasjoy104@gamil.com>
2024-06-07 23:53:33 +0600

    modal added

--- src/Components/Orders/OrderCreates.jsx
+++ src/Components/Orders/OrderCreates.jsx
@@ -1,17 +1,72 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ProductContext } from '../../Context/ProductProvider'
+import OrderVarients from './OrderVarients'
 
 const OrderCreates = () => {
 
     const {products} = useContext(ProductContext)
 
+
+  
+
+    useEffect(()=>{
+
+    },[])
+
   return (
+
+    
     <div className='flex justify-between'>
-    <label htmlFor="my_modal_6" className="btn px-10">Create</label>
-  <input type="checkbox" id="my_modal_6" className="modal-toggle" />
+         <label htmlFor="my_modal_6" className="btn px-10">Create</label>
+         <input type="checkbox" id="my_modal_6" className="modal-toggle" />
+        <div className="carousel w-full modal" role="dialog">
+      <div className='modal-box'>
+         
+       <div id="slide1" className="carousel-item relative w-full">
+         <img src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
+         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+           <a href="#slide4" className="btn btn-circle">❮</a> 
+           <a href="#slide2" className="btn btn-circle">❯</a>
+         </div>
+       </div> 
+       <div id="slide2" className="carousel-item relative w-full">
+         <img src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
+         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+           <a href="#slide1" className="btn btn-circle">❮</a> 
+           <a href="#slide3" className="btn btn-circle">❯</a>
+         </div>
+       </div> 
+       <div id="slide3" className="carousel-item relative w-full">
+         <img src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
+         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+           <a href="#slide2" className="btn btn-circle">❮</a> 
+           <a href="#slide4" className="btn btn-circle">❯</a>
+         </div>
+       </div> 
+       <div id="slide4" className="carousel-item relative w-full">
+         <img src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
+         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+           <a href="#slide3" className="btn btn-circle">❮</a> 
+           <a href="#slide1" className="btn btn-circle">❯</a>
+         </div>
+       </div>
+       <div className="modal-action">
+  <label htmlFor="my_modal_6" className="btn">Cancel</label>
+  <OrderVarients ></OrderVarients>
+</div>
+     </div>
+      </div>
+
+
+
+
+
+
+    {/* <label htmlFor="my_modal_6" className="btn px-10">Create</label>
+  <input type="checkbox" id="my_modal_6" className="modal-toggle" /> */}
 <div className="modal" role="dialog">
 <div className="modal-box">
-<h3 className="font-semibold text-center text-lg mb-10">Product (Create/View/Edit)</h3>
+<h3 className="font-semibold text-center text-lg mb-10">Order(Create)</h3>
 <div >
       <div className="overflow-x-auto">
   <table className="table table-zebra">
@@ -22,8 +77,8 @@ const OrderCreates = () => {
         <th>Name</th>
         <th>Brand</th>
         <th>Type</th>
-        <th>Created At</th>
-        <th>Actions</th>
+        <th>Select</th>
+       
       </tr>
     </thead>
   
@@ -35,8 +90,8 @@ const OrderCreates = () => {
           <td>{product.name}</td>
           <td>{product.brand}</td>
           <td>{product.type}</td>
-          <td>{product.created_at}</td>
-          <td>view | Edit | Delete</td>
+        
+          <td>+</td>
           </tr>
         )
       
@@ -51,8 +106,9 @@ const OrderCreates = () => {
 
 <div className="modal-action">
   <label htmlFor="my_modal_6" className="btn">Cancel</label>
-  <label htmlFor="my_modal_6" className="btn">Submit</label>
+  <OrderVarients ></OrderVarients>
 </div>
+
 
 </div>
 </div>
